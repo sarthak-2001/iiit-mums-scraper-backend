7189916ea5483a6f23a533a0cae851e6671e05f8
barbie17 <64645721+barbie17@users.noreply.github.com>
2020-05-27 03:15:02 +0530

    view grades and course_id files added

---
+++ mums/functions/course_id.js
@@ -0,0 +1,65 @@
+const rp = require("request-promise");
+const { login } = require("./login");
+const cheerio = require("cheerio");
+//const db = require("../firebase/firebaseInit");
+
+let course_data = async function (uid, pwd) {
+    let user = await login(uid, pwd);
+    let cookie = user.cookie;  
+    let option = {
+        url: "https://hib.iiit-bh.ac.in/m-ums-2.0/app.acadstu/myCourses/docList1.php",
+        simple: false,
+        resolveWithFullResponse: true,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+            Connection: "keep-alive",
+            Referer: "https://hib.iiit-bh.ac.in/m-ums-2.0/start/here/aisMenu.php",
+            Cookie: cookie 
+    
+        }
+    }
+    let data = { Course_info: [] };
+    let res = await rp.get(option) ;
+    const $ = cheerio.load(res.body) ;
+
+    $("tbody")
+        .children()
+        .each((i,ele) => {
+            const sem = $(ele)
+                .find("td")
+                .eq(0)
+                .text() 
+            const subject = $(ele)
+                .find("td")
+                .eq(1)
+                .text()
+                .replace(/^\s+|\s+$/g , "")
+            const credit = $(ele)
+                .find("td")
+                .eq(2)
+                .text()
+                .replace(/^\s+|\s+$/g , "")
+            const coid = $(ele)
+                .find("a")
+                .attr("href")
+                .split("=")[1]    
+                .replace(/^\s+|\s+$/g , "")                     //OR slice(17)
+            const course_link = $(ele)
+                .find("a")
+                .attr("href")
+                .replace(/^\s+|\s+$/g , "")
+            data.Course_info.push({
+                sem,
+                subject,
+                credit,
+                coid,
+                course_link,
+            });
+        });
+    //console.log(data.Course_info);
+    return data;
+};
+
+
+//course_data("b418018","barbie17*");
+module.exports = course_data;
---
+++ mums/functions/view_grades.js
@@ -0,0 +1,86 @@
+const rp = require("request-promise");
+const { login } = require("./login");
+const cheerio = require("cheerio");
+const db = require("../firebase/firebaseInit");
+
+let grade_scraper = async function (uid, pwd) {
+  let user = await login(uid, pwd);
+  let cookie = user.cookie;
+
+  let option = {
+    url:
+      "https://hib.iiit-bh.ac.in/m-ums-2.0/app.acadstu/myCourses/docDet1.php?coid=3634",
+    simple: false,
+    resolveWithFullResponse: true,
+    headers: {
+      Cookie: cookie,
+      Referer:
+        "https://hib.iiit-bh.ac.in/m-ums-2.0/app.acadstu/myCourses/docList1.php",
+    },
+  };
+  let data = { Grades: [] };
+  rp.get(option)
+    .then((res1) => {
+      let option2 = {
+        url:
+          "https://hib.iiit-bh.ac.in/m-ums-2.0/app.acadstu/coVwGrade/docDet1.php",
+        simple: false,
+        resolveWithFullResponse: true,
+        headers: {
+          Cookie: cookie,
+          Referer:
+            "https://hib.iiit-bh.ac.in/m-ums-2.0/app.acadstu/myCourses/docDet1.php?coid=3634",
+        },
+      };
+      rp.get(option2)
+        .then((res) => {
+            //console.log(res.body)
+            const $ = cheerio.load(res.body);
+            const student_id = $('p').children()
+                .eq(2)
+                .text()
+            const student_name = $('p').children()
+                .eq(5)
+                .text()
+            const quiz_1 = $('p').children()
+                .eq(8)
+                .text()
+            const quiz_2 = $('p').children()
+                .eq(11)
+                .text()
+            const midsem = $('p').children()
+                .eq(14)
+                .text()
+            const ta = $('p').children()
+                .eq(17)
+                .text()
+            const endsem = $('p').children()
+                .eq(20)
+                .text()
+            const grade_points = $('p').children()
+                .eq(23)
+                .text()
+            const grade = $('p').children()
+                .eq(26)
+                .text()
+            data.Grades.push({
+                student_id,
+                student_name,
+                quiz_1,
+                quiz_2,
+                midsem,
+                ta,
+                endsem,
+                grade_points,
+                grade
+            })
+            //console.log(data.Grades)
+            return data;
+        })
+        .catch((e) => console.log(e));
+    })
+    .catch((e) => console.log(e));
+};
+
+//scraper("b418045", "kitu@2001");
+module.exports = grade_scraper;
