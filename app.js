6419f59c05ad8b608c243a3e0f49e9ac468161d4
Sarthak Brahma <sarthakbrahma2001@gmail.com>
2020-06-01 18:41:19 +0530

    fixed app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const loginRouter = require("./mums/routes/login");
 const noticeRouter = require("./mums/routes/notices");
 const gradesRouter = require("./mums/routes/view_grades");
+const studentRouter = require("./mums/routes/stuSearch");
+const studentImageRouter = require("./mums/routes/stuImage");
+const facultyRouter = require("./mums/routes/facSearch");
+const facListRouter = require("./mums/routes/facList");
+const facultyImageRouter = require("./mums/routes/facImage");
+const intraRouter = require("./mums/routes/intraNotices");
+const bookRouter = require("./mums/routes/booksearch");
+
 
 const app = express();
 
@@ -10,6 +18,13 @@ app.use(express.json());
 app.use(loginRouter);
 app.use(noticeRouter);
 app.use(gradesRouter);
+app.use(studentRouter);
+app.use(studentImageRouter);
+app.use(facultyRouter);
+app.use(facListRouter);
+app.use(facultyImageRouter);
+app.use(intraRouter);
+app.use(bookRouter);
 
 const PORT = process.env.PORT || 3001;
 
